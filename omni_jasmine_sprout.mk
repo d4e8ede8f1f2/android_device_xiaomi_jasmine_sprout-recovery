74df31a1237525613ed2ef756ab0c2b4f56f7932
d4e8ede8f1f2 <d4e8ede8f1f2@gmail.com>
2019-06-24 08:50:01 +0300

    release

---
+++ omni_jasmine_sprout.mk
@@ -0,0 +1,44 @@
+
+# Release name
+PRODUCT_RELEASE_NAME := jasmine_sprout
+
+# Inherit some common Omni stuff.
+$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := jasmine_sprout
+PRODUCT_NAME := omni_jasmine_sprout
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Mi A2
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jasmine_sprout \
+    BUILD_PRODUCT=jasmine_sprout \
+    TARGET_DEVICE=jasmine_sprout
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.bionic.ld.warning=0 \
+    ro.treble.enabled=true
+
+# A/B updater
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
---
+++ recovery/root/sbin/link-to-linker64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+relink()
+{
+ cp $1 ${1}_old
+ sed 's|/system/bin/linker64|///////sbin/linker64|' "${1}_old" > "$1"
+ chmod 755 $1
+ rm ${1}_old
+}
+
+relink android.hardware.boot@1.0-service
+relink android.hardware.gatekeeper@1.0-service-qti
+relink android.hardware.keymaster@3.0-service-qti
+relink qseecomd
+relink time_daemon
+
+exit 0
